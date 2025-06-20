d0c4ede5a7b1af4702c8a9716f4e8d36a4c19e20
Bella (Isobel) <121617711+bgsbgsbgs42@users.noreply.github.com>
2025-06-20 11:17:20 +0100

    completing remainder of a and printing the df using df.shape

--- PartTwo.py
+++ PartTwo.py
@@ -2,11 +2,23 @@
 import pandas as pd
 
 def data_processing():
-    
-    # Read the CSV file
+    # Reading the CSV file
     df = pd.read_csv('texts/hansard40000.csv')
     
-    #Rename Labour (Co-op) to Labour
+    #Renaming Labour (Co-op) to Labour (i)
     df['party'] = df['party'].replace('Labour (Co-op)', 'Labour')
     
+    #Keeping only the four most common parties and removing Speaker column (ii)
+    top_parties = df['party'].value_counts().nlargest(4).index
+    df = df[df['party'].isin(top_parties)]
+    df = df[df['party'] != 'Speaker']
+    
+    #Keeping only the rows where speech_class is Speech (iii)
+    df = df[df['speech_class'] == 'Speech']
+    
+    #Removing rows with speeches shorter than 1000 characters (iv)
+    df = df[df['speech'].str.len() >= 1000]
+    
+    #Printing the dimensions of the resulting dataframe using df.shape
+    print(df.shape)
     
